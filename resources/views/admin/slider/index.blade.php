92b32342cb43efc0873b1a48716376bf7ce06a98
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-02-19 07:45:25 +0200

    Add Home Slider- Creating Necessary files

---
+++ resources/views/admin/slider/index.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Slider</h1>
+        </div>
+        <div class="card card-secondary">
+            <div class="card-header">
+                <h4>Card Header</h4>
+                <div class="card-header-action">
+                    <a href="#" class="btn btn-primary">View All</a>
+                    <a href="#" class="btn btn-danger">Delete All</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <p>Write something here</p>
+            </div>
+        </div>
+    </section>
+@endsection
