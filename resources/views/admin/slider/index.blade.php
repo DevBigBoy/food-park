e39fa9bb5c88526a448f23dfc23da1921b6e71c2
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-02-21 04:45:25 +0200

    Add Home Slider - Create Slider Feature (Part - 1)

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -5,9 +5,14 @@
         <div class="section-header">
             <h1>Slider</h1>
         </div>
-        <div class="card card-secondary">
+        <div class="card card-primary">
             <div class="card-header">
-
+                <h4>Card Header</h4>
+                <div class="card-header-action">
+                    <a href="{{ route('admin.slider.create') }}" class="btn btn-primary">
+                        Create Slider
+                    </a>
+                </div>
             </div>
             <div class="card-body">
                 {{ $dataTable->table() }}
